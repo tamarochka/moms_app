22de32bd9982a891c523f5ed81fe6e6c4e614fff
tamarochka <tamara.zenkevich@gmail.com>
2015-09-03 21:55:29 -0400

    changed DOB into yesrs to be displayed

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -1,4 +1,11 @@
 class Baby < ActiveRecord::Base
  validates :first_name, presence: true
  validates :dob, presence: true
+
+  def age
+    now = Time.now.utc.to_date
+    now.year - dob.year - (dob.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
+
+
 end
